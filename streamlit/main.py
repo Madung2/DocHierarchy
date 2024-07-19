bc0976d5ece4bce22a911b07b2682830e37729ea
Madung2 <tuliphan91@gmail.com>
2024-07-19 13:35:43 +0900

    hwp converter applied

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,36 +1,56 @@
 
 
+import os
 import streamlit as st
 from parser import DocxParser
-
+from converter.docx_converter import convert_to_docx
 
 st.title("docx 문서 구조화 모듈")
 st.write("----------------------")
 
-uploaded_file = st.file_uploader("Choose a file", type=['docx', 'doc'])
+uploaded_file = st.file_uploader("Choose a file", type=['docx', 'doc', 'hwp'])
 st.write("----------------------")
 
-html_code =""
+html_code = ""
+content = {}
 if uploaded_file is not None:
     print(uploaded_file, type(uploaded_file))
-    if uploaded_file.type != "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        content = {"error": "Invalid file type"}
-    parser = DocxParser(uploaded_file)
-    content = parser.parse()
-    content = parser.assign_levels(content)
-    content = parser.remove_textless_content(content)
-    ### tab 위치 지정
-    content = parser.get_x_pos_level(content)
-    parser.put_table_level(content)
-    ### tab 위치 지정
-    html_code = parser.generate_html(content)
-    print(content)
 
-    content = parser.build_tree(content)
+    # Create temp directory if it doesn't exist
+    temp_dir = os.path.join("streamlit", "converter", "converted_files")
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    # Save uploaded file to a temporary location
+    temp_input_file = os.path.join(temp_dir, uploaded_file.name)
+    with open(temp_input_file, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    output_dir = temp_dir
+
+    # Check file type and convert if necessary
+    print('type', uploaded_file.type)
+    if uploaded_file.type in ["application/x-hwp", "application/haansofthwp"]:
+        temp_input_file = convert_to_docx(temp_input_file, output_dir)
+
+    # Check if the file is a valid DOCX file
+    if not temp_input_file.endswith(".docx"):
+        content = {"error": "Invalid file type"}
+    else:
+        parser = DocxParser(temp_input_file)
+        content = parser.parse()
+        content = parser.assign_levels(content)
+        content = parser.remove_textless_content(content)
+        ### tab 위치 지정
+        content = parser.get_x_pos_level(content)
+        parser.put_table_level(content)
+        ### tab 위치 지정
+        html_code = parser.generate_html(content)
+        print(content)
+
+        content = parser.build_tree(content)
 
 else:
-    content ={"hello": "World!"}
-
+    content = {"hello": "World!"}
 
 tabs = ["HTML", "JSON"]
 
